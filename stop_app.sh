21e1ebd47233b78e5f5e7c3ad36b526c5f8150d7
sukant-kumar <sukant.kumar@arrise.com>
2026-05-06 21:12:00 +0530

    added a bash script to remove all containers and images related to application

---
+++ stop_app.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+application_container_name="techbuddy_app"  # This should match the container name in docker-compose.yml
+application_image_name="techbuddy-app:latest"  # This should match the image name in docker-compose.yml
+postgres_container_name="techbuddy_postgres"  # This should match the container name in docker-compose.yml
+postgres_image_name="postgres:latest"  # This should match the image name in docker-compose.yml
+
+echo "[~] Stopping TechBuddy application container: $application_container_name"
+# Stop the application container
+docker stop $application_container_name
+if [ $? -eq 0 ]; then
+    echo "[+] Successfully stopped the application container."
+else
+    echo "[-] Failed to stop the application container. It may not be running."
+fi
+
+echo "[~] Removing TechBuddy application container: $application_container_name"
+# Remove the application container
+docker rm $application_container_name
+if [ $? -eq 0 ]; then
+    echo "[+] Successfully removed the application container."
+else
+    echo "[-] Failed to remove the application container. It may have already been removed."
+fi
+
+echo "[~] Removing TechBuddy application image: $application_image_name"
+# Remove the associated images
+docker rmi $application_image_name
+if [ $? -eq 0 ]; then
+    echo "[+] Successfully removed the application image."
+else
+    echo "[-] Failed to remove the application image. It may have already been removed."
+fi
+
+echo "[~] Stopping PostgreSQL container: $postgres_container_name"
+# Stop the PostgreSQL container
+docker stop $postgres_container_name
+if [ $? -eq 0 ]; then
+    echo "[+] Successfully stopped the PostgreSQL container."
+else
+    echo "[-] Failed to stop the PostgreSQL container. It may not be running."
+fi
+
+echo "[~] Removing TechBuddy PostgreSQL container: $postgres_container_name"
+# Remove the PostgreSQL container
+docker rm $postgres_container_name
+if [ $? -eq 0 ]; then
+    echo "[+] Successfully removed the PostgreSQL container."
+else
+    echo "[-] Failed to remove the PostgreSQL container. It may have already been removed."
+fi
+
+echo "[~] Removing TechBuddy PostgreSQL image: $postgres_image_name"
+# Remove the associated images
+docker rmi $postgres_image_name
+if [ $? -eq 0 ]; then
+    echo "[+] Successfully removed the PostgreSQL image."
+else
+    echo "[-] Failed to remove the PostgreSQL image. It may have already been removed."
+fi
